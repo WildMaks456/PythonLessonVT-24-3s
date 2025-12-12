9aafac6ae12e1de2fa175614e4b75ca84b3b875d
Никита Царегородцев <tsareg.n@gmail.com>
2025-12-12 13:18:25 +0500

    ☺

--- test.py
+++ test.py
@@ -1,43 +1,117 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
-#3 вариант
+from PyQt5.QtWidgets import *
+#3 задание 1 ☺
 
-
-
-class FromProduct:
-    
+class FormProduct:
     def __init__(self):
         self.layout = QVBoxLayout()
 
-    def add_widget(self,widget):
+    def add_widget(self, widget):
         self.layout.addWidget(widget)
 
-    class FromDirector:
 
-        def __init__(self,builder):
-            self.builder = builder
+class FormBuilder:
+    def __init__(self):
+        self.product = FormProduct()
+    
+    def add_title(self):
+        pass
+    
+    def add_inputs(self):
+        pass
+    
+    def add_controls(self):
+        pass
+    
+    def get_result(self):
+        return self.product
 
-        def build_form(self):
-            self.builder.add_title()
-            self.builder.add_inputs()
-            self.builder.add_controls()
-            return self.builder.get_result()
+
+class MiniFormBuilder(FormBuilder):
+    def add_title(self):
+        label = QLabel("Мини-форма")
+        self.product.add_widget(label)
+    
+    def add_inputs(self):
+        edit = QLineEdit()
+        edit.setPlaceholderText("мини ввод")
+        self.product.add_widget(edit)
+    
+    def add_controls(self):
+        btn = QPushButton("посчитать")
+        self.product.add_widget(btn)
+
+
+class FullFormBuilder(FormBuilder):
+    def add_title(self):
+        label = QLabel("Расширенная форма")
+        self.product.add_widget(label)
+    
+    def add_inputs(self):
+        self.product.add_widget(QLabel("Цена:"))
+        self.product.add_widget(QLineEdit())
+        self.product.add_widget(QLabel("Процент:"))
+        self.product.add_widget(QLineEdit())
+    
+    def add_controls(self):
+        hbox = QHBoxLayout()
+        hbox.addWidget(QPushButton("Посчитать"))
         
-        
+        widget = QWidget()
+        widget.setLayout(hbox)
+        self.product.add_widget(widget)
+
+
+class FormDirector:
+    def __init__(self, builder):
+        self.builder = builder
+    
+    def build_form(self):
+        self.builder.add_title()
+        self.builder.add_inputs()
+        self.builder.add_controls()
+        return self.builder.get_result()
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-
+        self.setWindowTitle("bob the builder")
+        self.setGeometry(100, 100, 400, 400)
         
-        self.setCentralWidget(button)
-
+        layout = QVBoxLayout()
+        
+        layout.addWidget(QLabel("миНИ"))
+        mini_builder = MiniFormBuilder()
+        director = FormDirector(mini_builder)
+        mini_product = director.build_form()
+        mini_widget = QWidget()
+        mini_widget.setLayout(mini_product.layout)
+        layout.addWidget(mini_widget)
+        
+        layout.addWidget(QLabel(" Расширенная форма"))
+        full_builder = FullFormBuilder()
+        director = FormDirector(full_builder)
+        full_product = director.build_form()
+        full_widget = QWidget()
+        full_widget.setLayout(full_product.layout)
+        layout.addWidget(full_widget)
+        
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
+sys.exit(app.exec())
 
-app.exec()
+
+
+
+
+
+
+
+
+
